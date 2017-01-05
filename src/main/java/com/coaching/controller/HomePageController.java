9d669b07b0977c7a1c896ab6ef276ae4a2cfd035
Syed Naim <syednaim@outlook.com>
2017-01-04 21:16:53 -0500

    clean up, removed target folder

--- src/main/java/com/coaching/controller/HomePageController.java
+++ src/main/java/com/coaching/controller/HomePageController.java
@@ -1,11 +1,23 @@
 package com.coaching.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * Created by Aknaim on 2017-01-04.
  */
 
 @Controller
+@RequestMapping(value = "lol")
 
 public class HomePageController {
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getHomePage(){
+        return new ModelAndView("index.html");
+    }
 
 }
